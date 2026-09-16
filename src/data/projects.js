b1c3c17de510c5dd7c73646eecf93535a4a44cf5
Naima Malik <noma_m@hotmail.com>
2026-09-16 10:33:54 +0200

    Change back to original URL for dance website.

--- src/data/projects.js
+++ src/data/projects.js
@@ -53,7 +53,7 @@ export const projects = [
         description:
             "A responsive dance academy website built with React and TypeScript, featuring dance classes, events, class details and contact information. I used React Router for navigation and Tailwind CSS for styling and responsive design.",
         technologies: ["React", "TypeScript", "React Router", "Tailwind CSS"],
-        url: "https://booking-dance-clases-web-application-v2-9kchiwjv0.vercel.app",
+        url: "https://booking-dance-clases-web-applicatio.vercel.app/",
         github: "https://github.com/bdnaima/booking-dance-clases-web-application-v2",
     },
 
